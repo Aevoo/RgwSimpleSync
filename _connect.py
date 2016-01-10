073e471abe92fcf9cb34940cf00fb891b25719fe
Dcasier <david.casier@aevoo.fr>
2016-01-10 18:14:20 +0100

    Some mistake

--- _connect.py
+++ _connect.py
@@ -2,34 +2,11 @@ import boto
 import boto.s3.connection
 import json
 
-
-""" default.json sample
-{
-  "source": {
-    "urls": {
-      "url1":"http://localhost:80",
-      "url2":"http://localhost:81",
-      "url3":"http://localhost:82"
-    },
-    "access_key": "XXXXXXXXXXXXXXXXXXXXX",
-    "secret_key": "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-  },
-  "destination": {
-    "urls": {
-      "url1":"http://localhost:8080"
-    },
-    "access_key": "XXXXXXXXXXXXXXXXXXXXX",
-    "secret_key": "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-  }
-}
-"""
 conf=json.load(open('/etc/ceph/RgwSimpleSync/default.json', 'r'))
 
-def get_rgw_source(source_or_dest,_id):
+def get_rgw_conn(source_or_dest,_id):
   _s_conf=conf[source_or_dest]
   _url=_s_conf["urls"]["url"+str(_id)]
-  print _s_conf["access_key"]
-  print _s_conf["secret_key"]
   return boto.connect_s3(
     aws_access_key_id    =_s_conf["access_key"],
     aws_secret_access_key=_s_conf["secret_key"],
--- _sync.py
+++ _sync.py
@@ -7,19 +7,18 @@ import time
 import traceback
 
 _num_procs=8
-_num_rgw=_connect.get_num_rgw()
+_num_rgw=_connect.get_num_rgw('source')
 _effect_num_procs=_num_procs*_num_rgw
 
-destination=_connect.get_rgw_conn("destination", 1)
-
+destination=_connect.get_rgw_conn("destination", 0)
 _list_buckets_dest=[k.name for k in destination.get_all_buckets()]
 
 def sync_bucket(i):
   source=_connect.get_rgw_conn("source", i%_num_rgw)
   position=0
   empty=False
-  _aevooys_dict=[]
-  _prodys_dict=[]
+  _dest_dict=[]
+  _sour_dict=[]
   while not empty:
     try:
       time.sleep(10)
@@ -27,29 +26,30 @@ def sync_bucket(i):
       position+=1
       if _next <= _list_buckets_dest.__len__():
         _bucket_dest=_list_buckets_dest[_next]
-        try:
-          _connect.aevooys.head_bucket(_bucket_dest)
-        except:
-          print _bucket_dest + ' does not exist'
-          continue
-        if _get_meta_buckets.is_bucket_sync('Prod', _bucket_dest):
+        if _get_meta_buckets.is_bucket_sync(_bucket_dest):
           print _bucket_dest + ' already sync'
           continue
         try:
-          _aevooys_dict=_get_meta_buckets.get_buckets_dict(destination, _bucket_dest)
-          _prodys_dict=_get_meta_buckets.load_buckets_dict('Prod', _bucket_dest)
+          destination.head_bucket(_bucket_dest)
+        except:
+          print traceback.print_exc()
+          print _bucket_dest + ' does not exist'
+          continue
+        try:
+          _dest_dict=_get_meta_buckets.get_buckets_dict(destination, _bucket_dest)
+          _sour_dict=_get_meta_buckets.load_buckets_dict(_bucket_dest)
         except:
           print traceback.print_exc()
           print _bucket_dest + ' Error get_buckets_dict'
           continue
-        _bucket=_get_meta_buckets.get_bucket_wrong_key_dict(_prodys_dict, _aevooys_dict)
+        _bucket=_get_meta_buckets.get_bucket_wrong_key_dict(_sour_dict, _dest_dict)
         _source_bucket=source.get_bucket(_bucket_dest)
         _dest_bucket=destination.get_bucket(_bucket_dest)
         for _key in _bucket[1]:
           _new_key=_dest_bucket.new_key(_key)
           _new_key.set_contents_from_string(_source_bucket.get_key(_key).get_contents_as_string())
         print _bucket_dest + ' is sync'
-        _get_meta_buckets.bucket_is_sync('Prod', _bucket_dest)
+        _get_meta_buckets.bucket_is_sync(_bucket_dest)
       else:
         empty=True
     except:
